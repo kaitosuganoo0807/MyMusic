e57dfd33d4509d1684b73cc3bc4ac05e62777c4f
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-03-12 07:11:03 +0900

    簡単な音楽アプリ

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    // シンバルの音源fファイルを指定
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    
+    // シンバル用のプレイヤーインスタンスを作成
+    var cymbalPlayer = AVAudioPlayer()
+    
+    // ギターの音源ファイルを指定
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    
+    // ギター用のプレイヤーインスタンスを作成
+    var guitarPlayer = AVAudioPlayer()
+    
+    // バックミュージックの音源ファイルを指定
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    
+    // バックミュージック用のプレイヤーインスタンスを作成
+    var backmusicPlayer = AVAudioPlayer()
 
-
+    @IBAction func cymbal(_ sender: Any) {
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
+    }
+    
+    
+    @IBAction func guitar(_ sender: Any) {
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
+    }
+    
+    
+    @IBAction func play(_ sender: Any) {
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        // バックミュージック停止
+        backmusicPlayer.stop()
+    }
+    
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラーが発生しました")
+        }
+    }
+    
+    
 }
 
